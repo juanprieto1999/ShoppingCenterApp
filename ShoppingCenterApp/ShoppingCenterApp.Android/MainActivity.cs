fd56b9dc6ecb0465f26b3c4bc5d4851225bd41b1
whitecamilo_33@hotmail.com <camilo181995>
2018-10-04 17:28:19 -0500

    splash

--- ShoppingCenterApp/ShoppingCenterApp.Android/MainActivity.cs
+++ ShoppingCenterApp/ShoppingCenterApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ShoppingCenterApp.Droid
 {
-    [Activity(Label = "ShoppingCenterApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ShoppingCenterApp", Icon = "@drawable/Icono", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
