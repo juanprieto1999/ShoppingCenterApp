d604fcc84301b62fffbee083a5c890024e37a13a
juanprieto1999 <41449371+juanprieto1999@users.noreply.github.com>
2018-10-04 16:38:22 -0500

    List View Carritos

---
+++ ShoppingCenterApp/ShoppingCenterApp/CarItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoppingCenterApp
+{
+    class CarItem
+    {
+        public string Titulo { get; set; }
+        public string Icono { get; set; }
+        public string Detalle { get; set; }
+        public int Precio { get; set; }
+    }
+}
